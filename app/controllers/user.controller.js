e104fe9df383a67ca82374936a7d520945d14af1
likhithabattula <166478299+likhithabattula@users.noreply.github.com>
2024-06-04 00:00:46 -0400

    update and delete for story controller is added

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -75,7 +75,7 @@ exports.create = async (req, res) => {
                 email: user.email,
                 first_name: user.first_name,
                 last_name: user.last_name,
-                id: user.id,
+                id: userId,
                 token: token,
                 is_admin: user.is_admin
               };
