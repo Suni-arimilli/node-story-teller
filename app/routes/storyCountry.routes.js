65152272c4a2c69dea6b4fa4e3216effb6e714aa
pranavi-k <ramapranavi.koganti@eagles.oc.edu>
2024-06-13 14:01:12 -0400

    Add narratvies,country and role routes and controllers

--- app/routes/storyCountry.routes.js
+++ app/routes/storyCountry.routes.js
@@ -1,8 +1,12 @@
 module.exports = (app) => {
-    const storyCountryController = require("../controllers/storyCountry.controller.js");
-    var router = require("express").Router();
-  
-    router.get("/", storyCountryController.findAll);
-    app.use("/storyCountries", router);
-  };
-  
+  const storyCountryController = require("../controllers/storyCountry.controller.js");
+  var router = require("express").Router();
+
+  router.post("/", storyCountryController.create);
+  router.get("/", storyCountryController.findAll);
+  router.get("/:id", storyCountryController.findOne);
+  router.put("/:id", storyCountryController.update);
+  router.delete("/:id", storyCountryController.delete);
+  router.delete("/", storyCountryController.deleteAll);
+  app.use("/storyCountries", router);
+};
--- app/routes/storyRole.routes.js
+++ app/routes/storyRole.routes.js
@@ -1,8 +1,12 @@
 module.exports = (app) => {
-    const storyRoleController = require("../controllers/storyRole.controller.js");
-    var router = require("express").Router();
-  
-    router.get("/", storyRoleController.findAll);
-    app.use("/storyRoles", router);
-  };
-  
+  const storyRoleController = require("../controllers/storyRole.controller.js");
+  var router = require("express").Router();
+
+  router.post("/", storyRoleController.create);
+  router.get("/", storyRoleController.findAll);
+  router.get("/:id", storyRoleController.findOne);
+  router.put("/:id", storyRoleController.update);
+  router.delete("/:id", storyRoleController.delete);
+  router.delete("/", storyRoleController.deleteAll);
+  app.use("/storyRoles", router);
+};
