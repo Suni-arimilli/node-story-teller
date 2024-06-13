95d022ff6ffa0bbad2725e7edda91b80788f252e
pranavi-k <ramapranavi.koganti@eagles.oc.edu>
2024-06-13 14:03:17 -0400

    Update database models to include isInUse for story settings

--- app/models/category.model.js
+++ app/models/category.model.js
@@ -1,15 +1,19 @@
 module.exports = (sequelize, Sequelize) => {
-    const Category = sequelize.define("category", {
-      id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        primaryKey: true
-      },
-      name: {
-        type: Sequelize.STRING(100),
-        allowNull: false
-      }
-    });
-    return Category;
-  };
-  
+  const Category = sequelize.define("category", {
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    name: {
+      type: Sequelize.STRING(100),
+      allowNull: false
+    },
+    isInUse: {
+      type: Sequelize.BOOLEAN,
+      allowNull: false,
+      defaultValue: true
+    }
+  });
+  return Category;
+};
--- app/models/configuration.model.js
+++ app/models/configuration.model.js
@@ -1,14 +1,19 @@
 module.exports = (sequelize, Sequelize) => {
-const Configuration = sequelize.define("configuration", {
-    id: {
-      type: Sequelize.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-    name: {
-      type: Sequelize.STRING(100),
-      allowNull: false,
-    }
-  });
-  return Configuration;
-};
+  const Configuration = sequelize.define("configuration", {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+      },
+      isInUse: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: true
+      }
+    });
+    return Configuration;
+  };
